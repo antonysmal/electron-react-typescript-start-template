b58a207442a17f3c258561f4d45ddb9b81a56271
antonysmal <antony.smal@gmail.com>
2025-04-07 23:38:42 +0300

    Content Security Policy (CSP) added through webpack

--- webpack.renderer.config.ts
+++ webpack.renderer.config.ts
@@ -4,6 +4,8 @@ import HtmlWebpackPlugin from 'html-webpack-plugin';
 import { rules } from './webpack.rules';
 import { plugins } from './webpack.plugins';
 
+const isDev = process.env.NODE_ENV !== 'production';
+
 rules.push({
   test: /\.css$/,
   use: ['style-loader', 'css-loader'],
@@ -12,6 +14,14 @@ rules.push({
 plugins.push(
   new HtmlWebpackPlugin({
     template: './src/renderer/index.html',
+    meta: {
+      'Content-Security-Policy': {
+        'http-equiv': 'Content-Security-Policy',
+        content: isDev
+          ? "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; img-src 'self' data:;"
+          : "default-src 'self'; script-src 'self'; style-src 'self'; img-src 'self' data:;"
+      }
+    }
   })
 );
 
